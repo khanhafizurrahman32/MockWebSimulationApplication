8202e917fc8a287968b9c2f7db5ad411b07ba041
khanhafizurrahman32 <khanhafizurrahman32@gmail.com>
2018-01-06 21:20:34 +0100

    separate drawmoldDirective files into upperMoldDirective & lower directive

---
+++ js/LowerMoldDirective.js
@@ -0,0 +1,3 @@
+/**
+ * Created by khanhafizurrahman on 1/6/18.
+ */
---
+++ js/upperMoldDirective.js
@@ -0,0 +1,17 @@
+/**
+ * Created by khanhafizurrahman on 1/6/18.
+ */
+
+var upperMoldDirective = function(){
+    var configurationForUpperMold = function () {
+
+    }
+
+    var drawUpperMold = function () {
+
+    }
+    return {
+        configurationForUpperMold: configurationForUpperMold,
+        drawUpperMold: drawUpperMold
+    }
+}
